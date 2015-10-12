08f31a63f998c9b117ebbf2fe77dae788b65d887
Juan Wajnerman <jwajnerman@manas.com.ar>
2015-10-11 22:18:24 -0300

    Show syntax errors

--- crystal_format.py
+++ crystal_format.py
@@ -4,6 +4,7 @@ import subprocess
 import tempfile
 import os
 import difflib
+import json
 
 class CrystalPluginListener(sublime_plugin.EventListener):
   def on_pre_save(self, view):
@@ -19,23 +20,36 @@ class CrystalFormatCommand(sublime_plugin.TextCommand):
     vsize = self.view.size()
     region = sublime.Region(0, vsize)
     src = self.view.substr(region)
-
-    with tempfile.NamedTemporaryFile(mode = 'w+t', delete = False) as tmp:
-      tmp.write(src)
+    window = self.view.window()
 
     settings = sublime.load_settings('Crystal.sublime-settings')
-    subprocess.call([settings.get("crystal_cmd"), "tool", "format", tmp.name])
+    with subprocess.Popen([settings.get("crystal_cmd"), "tool", "format", "-", "--format", "json"], stdin = subprocess.PIPE, stdout = subprocess.PIPE) as proc:
+      proc.stdin.write(bytes(src, 'UTF-8'))
+      proc.stdin.close()
+      output = proc.stdout.read().decode('UTF-8')
+      exit = proc.wait()
 
-    with open(tmp.name, 'r') as formatted_file:
-      formatted = formatted_file.read()
-    os.unlink(tmp.name)
+    if exit == 0:
+      for op, i1, i2, j1, j2 in difflib.SequenceMatcher(None, src, output).get_opcodes():
+        if op == 'insert':
+          self.view.insert(edit, j1, output[j1:j2])
+          next
+        if op == 'delete':
+          self.view.erase(edit, sublime.Region(j1, j1 + (i2 - i1)))
+          next
+        if op == 'replace':
+          self.view.replace(edit, sublime.Region(j1, j1 + (i2 - i1)), output[j1:j2])
+      self.view.erase_regions('crystal_errors')
+      window.run_command("hide_panel")
 
-    for op, i1, i2, j1, j2 in difflib.SequenceMatcher(None, src, formatted).get_opcodes():
-      if op == 'insert':
-        self.view.insert(edit, j1, formatted[j1:j2])
-        next
-      if op == 'delete':
-        self.view.erase(edit, sublime.Region(j1, j1 + (i2 - i1)))
-        next
-      if op == 'replace':
-        self.view.replace(edit, sublime.Region(j1, j1 + (i2 - i1)), formatted[j1:j2])
+    else:
+      error = json.loads(output)
+      error_pos = self.view.text_point(error[0]["line"] - 1, error[0]["column"] - 1)
+      line_region = self.view.full_line(error_pos)
+      self.view.add_regions('crystal_errors', [line_region], 'comment', 'dot')
+
+      error_panel = window.create_output_panel('crystal_errors')
+      error_panel.run_command("update", {"buffer":
+        "Error at line %d, column %d: %s" % (error[0]["line"], error[0]["column"], error[0]['message'])
+      })
+      window.run_command("show_panel", {"panel": "output.crystal_errors"})
